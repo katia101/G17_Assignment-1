f2847f80b9c5898466feeafeba518d8acf5e7478
katia101 <217149073@mycput.ac.za>
2021-04-01 14:10:52 +0200

    program commit

--- src/main/java/za/ac/cput/Assignment1.java
+++ src/main/java/za/ac/cput/Assignment1.java
@@ -1,4 +1,45 @@
 package za.ac.cput;
 
 public class Assignment1 {
+    private int birthYear;
+    private int currentYear;
+    private int age;
+
+    public Assignment1(int birthYear, int currentYear)
+    {
+        this.birthYear = birthYear;
+        this.currentYear = currentYear;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getCurrentYear() {
+        return currentYear;
+    }
+
+    public void setCurrentYear(int currentYear) {
+        this.currentYear = currentYear;
+    }
+
+    public int age(int currentAge){
+
+        currentAge = currentYear - birthYear;
+
+        return currentAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Assignment1{" +
+                "birthYear=" + birthYear +
+                ", currentYear=" + currentYear +
+                ", age=" + age +
+                '}';
+    }
 }
--- src/main/java/za/ac/cput/Mandisa.java
+++ src/main/java/za/ac/cput/Mandisa.java
@@ -1,4 +1,9 @@
 package za.ac.cput;
+import java.util.Scanner;
 
 public class Mandisa {
+
+
+
 }
+
