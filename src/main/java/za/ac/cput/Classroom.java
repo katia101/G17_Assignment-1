26db1fb70b0b2f6db5e9457c729c495d306523fe
PanasheM-design <PanasheM-design@users.noreply.github.com>
2021-04-01 20:08:38 +0200

    New additions

---
+++ src/main/java/za/ac/cput/Classroom.java
@@ -0,0 +1,19 @@
+package za.ac.cput;
+//Panashe Muinzani_218186568//
+
+public class Classroom {
+    private String name;
+
+    public Classroom(String name){
+        this.name = name;
+    }
+
+    public Classroom() {
+
+    }
+
+    public String getName(){
+        return name;
+    }
+}
+
